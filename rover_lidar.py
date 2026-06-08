2e1ab73bc8214af34afb45c16dae6764d7ea6c89
RUPENDRAN.M <rupendran2006@gmail.com>
2026-06-09 01:52:44 +0530

    Create rover_lidar.py

---
+++ rover_lidar.py
@@ -0,0 +1,9 @@
+import random
+
+class LiDAR:
+    def __init__(self):
+        print("📡 LiDAR initialized")
+
+    def get_distance(self):
+        # Simulated distance (replace with real LiDAR code)
+        return random.randint(10, 100)
